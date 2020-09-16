634adf946a863fb6ba347485af849095c1e5dbfc
Rogerio Alves <rogerioalves.ee@gmail.com>
2020-09-16 17:11:21 -0300

    Initial telemetry data model

--- api/models/telemetry-data.go
+++ api/models/telemetry-data.go
@@ -1,1 +1,14 @@
 package models
+
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
+// TelemetryData : PV system state captured by the data acquisition service
+type TelemetryData struct {
+	ID                primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`
+	Serial            string             `bson:"serial" json:"serial"`
+	Module            string             `bson:"module" json:"module"`
+	LastTelemetryTime int64              `bson:"lastTelemetryTime" json:"lastTelemetryTime"`
+	OutputVoltage     float64            `bson:"outputVoltage" json:"outputVoltage"`
+	InputVoltage      float64            `bson:"inputVoltage" json:"inputVoltage"`
+	InputCurrent      float64            `bson:"inputCurrent" json:"inputCurrent"`
+}
